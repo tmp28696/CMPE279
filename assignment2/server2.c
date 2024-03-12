5a8422c364025f18ca2bac2608f9f7776bdc8ac2
Mit Patel <tithi286@gmail.com>
2024-03-12 14:53:09 -0700

    adding assignments

---
+++ assignment2/server2.c
@@ -0,0 +1,30 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 80
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    int status = setuid(65534);
+    if (status != 0){
+        printf("Error!! exiting the server.");
+        exit(EXIT_FAILURE);
+    }
+    printf("Status: %d",status );
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+
+    return 0; 
+} 
